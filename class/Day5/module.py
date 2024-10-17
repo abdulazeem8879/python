72d4517f64bc1c38910c80cef8a11c94e6e3467e
abdulazeem8879 <abdulazeemkhan8879@gmail.com>
2024-10-17 14:59:07 +0530

    add

---
+++ class/Day5/module.py
@@ -0,0 +1,13 @@
+
+
+def greeting(name):
+    print('hello ' + name)
+    
+def add(num1,num2):
+     result=num1+num2
+     print(result)
+
+def mul(num1,num2):
+    result=num1*num2
+    print(result)
+
---
+++ class/Day5/test.py
@@ -0,0 +1,5 @@
+import module
+
+module.greeting("Abdul Azeeem")
+module.add(6,8)
+module.mul(4,5)
